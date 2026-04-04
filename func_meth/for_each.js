68e3a55a262e598f304085824c5ab1f6f2faa374
dev <vaidikhitendrachoudhary@gmail.com>
2026-04-04 14:43:29 +0530

    updating info with watching lec

--- func_meth/for_each.js
+++ func_meth/for_each.js
@@ -28,11 +28,29 @@ arr2.forEach((val) => {
     console.log(val);
 });
 
-let arr3 = ["mumbai", "Delhi", "PuNe"];
+let arr3 = ["mumbai", "Delhi", "Pune"];
 
 arr3.forEach((val)=> {
     console.log(val.toUpperCase());
 });
 
+// three pramaeters can be used in this foreach callbackfunction
+arr3.forEach((val,idx,arr)=> {  // first one is value, 2nd is index, 3rd is array itself
+    console.log(val.toUpperCase(),idx,arr);
+});
 
-// after this i will not save anything 
+// ------------------------------------------------------------------------------------------------
+// what is higher order function or method ???
+// the function who uses another fxn as parameter or return another fxn is called as higher order fxn
+// ------------------------------------------------------------------------------------------------
+
+
+
+// another type of using arrowfxn by passing fxn in callback
+let arr5=[12,34,56,78,34];
+
+let calcSqr = (num,idx) => {
+    console.log(idx,num*num);
+};
+
+arr5.forEach(calcSqr);
