ccb9fc73881dee0d597fdca8d6caef6e182fee59
dev <vaidikhitendrachoudhary@gmail.com>
2026-05-03 01:23:10 +0530

    did something unnoticable

---
+++ objects/app.js
@@ -0,0 +1,20 @@
+const student ={
+    fullname : "Vaidik Choudhary",
+    marks: 94.4,
+    printMarks  : function(){
+        console.log("marks =", this.marks);
+    },
+};
+
+const employee = {
+    calcTax1() {
+        console.log("tax rate is 10%");
+    },
+    calcTax2 : function () {
+        console.log("tax rate is 10%");
+    },
+};
+
+const karanArjun  = {
+    salary : 
+}
